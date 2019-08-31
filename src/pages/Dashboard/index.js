e85431613dca83b3b2582f2ac1f2f446fa67f52f
Adriano Neres <ah.driano@gmail.com>
2019-08-31 18:32:58 -0400

    chore: initial structure

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Background from '~/components/Background';
+
+// import { Container } from './styles';
+
+export default function Dashboard() {
+  return <Background />;
+}
+
+Dashboard.navigationOptions = {
+  tabBarLabel: 'Meetups',
+};
